5d957b31d91156f4bc1a404c42b0dfcf868af784
MaSteR_MinD1 <shehabbebo6060@gmail.com>
2022-11-02 19:40:24 +0200

    add basic assets and utils for further features support

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Text>test</Text>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ utils/ConstantValues.ts
@@ -0,0 +1,3 @@
+export const LANG_KEY = "NewsILang";
+export const ModeKey = "ThemeMode";
+export const Url = "https://www.googleapis.com/books/v1/volumes?q=flowers";
